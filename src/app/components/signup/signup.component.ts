f9bf86bf8aee06e9434d3023dc80ac411e4027e8
Anshumaan Singh <anshumaanborntolead@gmail.com>
2024-04-17 18:27:24 +0530

    Update signup.component.ts

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class SignupComponent implements OnInit {
   public signupform!:FormGroup;
 
 
-  constructor(private formbuilder: FormBuilder, private http: HttpClient) { }
+  constructor(private formbuilder: FormBuilder, private http: HttpClient, private router:Router) { }
 
   ngOnInit(): void {
     this.signupform=this.formbuilder.group({
@@ -22,10 +23,17 @@ export class SignupComponent implements OnInit {
     })
   }
   signup(){
-    alert(this.signupform.value.username+this.signupform.value.password)
-    this.signupform.reset()
-    
-
+    // alert(this.signupform.value.username+this.signupform.value.password)
+    this.http.post<any>("http://127.0.0.1:8000/api/add_user/",this.signupform.value)
+    .subscribe(res=>{
+      alert("User Registration Successfull!")
+      this.signupform.reset()
+      this.router.navigate([""])
+    },
+      err=>{
+        alert("Something Went Wrong!")
+      }
+    )
   }
 
 }
